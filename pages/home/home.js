6a8913c91cc241113fbd0b7be8d40e5dbd74a006
cccRaim <chen.raim@qq.com>
2018-07-12 11:19:05 +0800

    关注我们改造 & 增加给赞入口

--- pages/home/home.js
+++ pages/home/home.js
@@ -14,6 +14,25 @@ Page({
   goFeedback () {
     app.goFeedback()
   },
+  followUs () {
+    wx.setClipboardData({
+      data: 'zjutjh',
+      success(){
+        wx.showModal({
+          title: '提示',
+          icon: 'success',
+          showCancel: false,
+          content: '复制成功，粘贴至搜索栏关注我们'
+        })
+      }
+    })
+  },
+  donate () {
+    wx.navigateToMiniProgram({
+      appId: app.env('geizanAppId'),
+      path: 'pages/apps/largess/detail?id=wA3oQqX64Yg%3D'
+    })
+  },
   userBlockClick () {
     if (!this.data.userInfo) {
       return wx.navigateTo({
