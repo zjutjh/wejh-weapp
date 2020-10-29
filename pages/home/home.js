6e4a4ec300124d440024cbbd25f2638cf083a8e0
Bunny Wong <bunnnywong@gmail.com>
2020-10-29 21:05:14 +0800

    给赞小程序已不再可用，去掉其入口

--- pages/home/home.js
+++ pages/home/home.js
@@ -26,12 +26,12 @@ Page({
       }
     })
   },
-  donate () {
-    wx.navigateToMiniProgram({
-      appId: app.env('geizanAppId'),
-      path: 'pages/apps/largess/detail?id=wA3oQqX64Yg%3D'
-    })
-  },
+  // donate () {
+  //   wx.navigateToMiniProgram({
+  //     appId: app.env('geizanAppId'),
+  //     path: 'pages/apps/largess/detail?id=wA3oQqX64Yg%3D'
+  //   })
+  // },
   userBlockClick () {
     if (!this.data.userInfo) {
       return wx.navigateTo({
