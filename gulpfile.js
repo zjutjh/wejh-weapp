8c5671583a8720f2277adfc108676dbca7f63f42
Bunny Wong <bunnnywong@gmail.com>
2020-10-31 13:56:00 +0800

    使用 gulp 自动编译 scss

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// https://gist.github.com/jeromecoupe/0b807b0c1050647eb340360902c3203a
+
+// Load plugins
+const del = require("del");
+const gulp = require("gulp");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+
+// Clean assets
+function clean() {
+  return del(["./app.wxss"]);
+}
+
+// CSS task
+function css() {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(rename("app.wxss"))
+    .pipe(gulp.dest("./"))
+}
+
+// Watch files
+function watch() {
+  gulp.watch("./scss/**/*", css);
+}
+
+// define complex tasks
+const build = gulp.series(clean, gulp.parallel(css));
+
+// export tasks
+exports.clean = clean;
+exports.css = css;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
