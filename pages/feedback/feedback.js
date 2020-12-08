5a537cca9d0da12d2d7495f3f0e58332b88cd701
Lambozhuang <Lambozhuang@outlook.com>
2020-12-08 12:49:41 +0800

    format code

--- pages/feedback/feedback.js
+++ pages/feedback/feedback.js
@@ -4,21 +4,13 @@ Page({
   data: {
 
   },
-  onLoad() {},
+  onLoad() { },
   copy() {
     wx.showActionSheet({
       itemList: ['462530805（一群）', '282402782（二群）'],
       success({ tapIndex }) {
         wx.setClipboardData({
           data: groups[tapIndex],
-          // success() {
-          //   wx.showModal({
-          //   title: "提示",
-          //   icon: "success",
-          //   showCancel: false,
-          //   content: "复制成功",
-          // });
-          // },
         });
       },
     });
