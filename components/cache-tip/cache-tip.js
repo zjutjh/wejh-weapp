7b7761f553ddb47a8fce5e944134a408997a7591
Ethan Wong <e1hanW0ng@gmail.com>
2021-03-01 15:41:25 +0800

    更新时间1分钟之内点击底部提示不出现弹窗

--- components/cache-tip/cache-tip.js
+++ components/cache-tip/cache-tip.js
@@ -1,7 +1,5 @@
 import formatter from "../../utils/formatter";
 
-import logger from "../../utils/logger";
-
 import dayjs from "../../libs/dayjs/dayjs.min.js";
 import dayjs_duration from "../../libs/dayjs/plugin/duration.js";
 
@@ -41,26 +39,29 @@ Component({
       }
     },
     showHint() {
-      try {
-        const duration = dayjs
+      var duration = 0;
+
+      if (this.data.timestamp) {
+        duration = dayjs
           .duration(dayjs().diff(dayjs.unix(this.data.timestamp)))
           .asSeconds();
-
-        wx.reportAnalytics("cache_tip_click", {
-          content: this.data.text,
-          duration: duration,
-          title: this.data.title,
-        });
-      } catch (err) {
-        logger.error("cache-tip", "cache-tip埋点上报异常", err);
       }
 
-      wx.showModal({
-        title: `为什么我的${this.data.title}可能有延迟`,
-        content: `微精弘会展示出最近一次获得的${this.data.title}。当由于教务系统不稳定等原因无法获取最新的${this.data.title}时，请通过教务系统查询。`,
-        showCancel: false,
-        confirmText: "我知道了",
+      wx.reportAnalytics("cache_tip_click", {
+        content: this.data.text,
+        duration: duration,
+        title: this.data.title,
       });
+
+      // 数据更新时间距今大于一分钟显示红点
+      if (duration > 60) {
+        wx.showModal({
+          title: `为什么我的${this.data.title}可能有延迟`,
+          content: `微精弘会展示出最近一次获得的${this.data.title}。当由于教务系统不稳定等原因无法获取最新的${this.data.title}时，请通过教务系统查询。`,
+          showCancel: false,
+          confirmText: "我知道了",
+        });
+      }
     },
   },
 });
